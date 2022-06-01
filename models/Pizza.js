76fc8b73137d2aa9adacac71ee46bd1e7c2f2fa8
jennj <jennjenn93@gmail.com>
2022-06-01 15:06:31 -0500

    adding timestamp

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
 const PizzaSchema = new Schema({
     pizzaName: {
@@ -9,14 +10,15 @@ const PizzaSchema = new Schema({
     },
     createdAt: {
         type: Date,
-        default: Date.now
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal)
     },
     size: {
         type: String,
         default: 'Large'
     },
     toppings: [],
-    Comments: [
+    comments: [
         {
             type: Schema.Types.ObjectId,
             ref: 'Comment'
@@ -26,6 +28,7 @@ const PizzaSchema = new Schema({
 {
     toJSON: {
         virtuals: true,
+        getters: true
     },
     id: false
 }
